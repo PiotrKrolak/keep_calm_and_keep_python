61b88ade107f9faff7bad005f565c6385a5f7bfb
Piotr <krolak.piotr.kontakt@gmail.com>
2024-08-11 02:15:55 +0200

    dziedziczenie - the least

--- dziedziczenie.py
+++ dziedziczenie.py
@@ -54,3 +54,18 @@ class Student(Person):
 
 z = Student("Janusz", "Bak")
 z.printname()
+
+
+# Python also has a super() function that will make the child class inherit all the methods and properties from its parent:
+# By using the super() function, you do not have to use the name of the parent element, it will automatically inherit the methods and properties from its parent.
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+
+    
