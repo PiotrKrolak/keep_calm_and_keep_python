2d6eebb12fdae6a6ad1dccacc128490ffb56e3c7
Piotr <krolak.piotr.kontakt@gmail.com>
2024-08-13 10:42:30 +0200

    tools.Timer()

--- tools.py
+++ tools.py
@@ -1,6 +1,48 @@
-class timer(self,)
-    self.start = 0
-    self.stop = 0
+import time
 
-    def time(self.start, self.stop)
+class Timer:
+    def __init__(self, start = 0, stop = 0):
+        self.start = start
+        self.stop = stop
+
+    def time(self):
         return self.start - self.stop
+    
+
+t1 = Timer()
+
+#x = t1.time()
+
+'''
+y = time.time()
+
+print(y)
+
+z = time.time()
+
+print(z)
+
+a = z - y
+
+print(a)
+
+################################################
+
+b = time.thread_time()
+
+print(b)
+
+################################################
+
+'''
+y = time.time_ns()
+
+print(y)
+
+z = time.time_ns()
+
+print(z)
+
+a = (z - y)
+
+print(a)
